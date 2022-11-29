29331d3af90c47bb6ca3bfbbdb935b085a3174e2
LeeYooBin <santosroberdan25@gmail.com>
2022-11-29 10:29:35 -0300

    Making the Banner component responsive

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,24 +3,47 @@ import styled from "styled-components";
 import MainImage from "../../assets/banner.png";
 
 const Content = styled.div`
-  width: 88%;
+  width: 100%;
   position: relative;
 `;
 
 const Image = styled.img`
+  // width: 100%;
   width: 100%;
-  height: 288px;
+  // height: 288px;
+  height: 150px;
+
+  @media screen and (min-width: 500px) {
+    width: 100%;
+    height: 288px;
+  }
 `;
 
 const Title = styled.h1`
   position: absolute;
   z-index: 1;
   color: #d9d9d9;
-  font-size: 2.5rem;
-  width: 401px;
+  // font-size: 2.5rem;
+  font-size: 1.2rem;
+  // width: 401px;
+  width: 200px;
   height: 144px;
-  top: 75px;
-  left: 44px;
+  // top: 75px;
+  // left: 44px;
+  top: 25%;
+  left: 5%;
+
+  @media screen and (min-width: 500px) {
+    font-size: 2rem;
+    width: 320px;
+  }
+
+  @media screen and (min-width: 1000px) {
+    font-size: 2.5rem;
+    width: 401px;
+    top: 75px;
+    left: 44px;
+  }
 `;
 
 const Banner = () => (
