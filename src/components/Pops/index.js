de5933e41336897d5950e53d4ff47f2e9a3b6f7e
LeeYooBin <santosroberdan25@gmail.com>
2022-12-03 01:14:00 -0300

    Creating Pops component

---
+++ src/components/Pops/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Content = styled.div``;
+
+const Pops = () => {
+  return(
+    <Content>
+      
+    </Content>
+  );
+};
+
+export default Pops;
