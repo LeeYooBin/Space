389e2e07c81e6d387ea3ad1fc72e6c622a852314
LeeYooBin <santosroberdan25@gmail.com>
2022-12-06 02:39:07 -0300

    Turning the Button into a component

--- src/components/Pops/index.js
+++ src/components/Pops/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PopsImages from "../../utils/PopsImages";
+import Button from "../Button";
 
 const Content = styled.aside`
   display: flex;
@@ -38,44 +39,6 @@ const Image = styled.img`
   height: 100px;
 `;
 
-const Button = styled.button`
-  margin-top: 1.5rem;
-  background: transparent;
-  border: none;
-  color: #d9d9d9;
-  font-family: "GandhiSansRegular";
-  font-size: 16px;
-  width: 100%;
-  padding: 12px 16px;
-  position: relative;
-  z-index: 0;
-  cursor: pointer;
-
-  :hover {
-    border-radius: 10px;
-    background: linear-gradient(to right, #c98cf1, #7b78e5) border-box;
-    color: #041833;
-    transition: 0.1s;
-  }
-
-  ::before {
-    content: "";
-    position: absolute;
-    inset: 0;
-    border-radius: 10px;
-    z-index: -1;
-    border: 2px solid transparent;
-    background: linear-gradient(to right, #c98cf1, #154580) border-box;
-    -webkit-mask: linear-gradient(#fff 0 0) padding-box,
-      linear-gradient(#fff 0 0);
-    -webkit-mask-composite: xor;
-    mask-composite: exclude;
-    background: linear-gradient(to right, #c98cf1, #7b78e5) border-box;
-    color: #041833;
-    transition: 0.1s;
-  }
-`;
-
 const Pops = () => (
   <Content>
     <H2>Popular</H2>
@@ -86,7 +49,7 @@ const Pops = () => (
         </Item>
       ))}
     </List>
-    <Button type="button">Load more photos</Button>
+    <Button text="Load more photos" />
   </Content>
 );
 
